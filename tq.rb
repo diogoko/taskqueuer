55ea52f0947e6034d12c1472cb48983f3d1a9257
Diogo Kollross <diogoko@gmail.com>
2014-05-06 22:06:53 -0300

    Refactored to export the start and end dates of each activity.

--- tq.rb
+++ tq.rb
@@ -25,21 +25,25 @@ require 'singleton'
 
 
 class Task
-  attr_accessor :description, :effort
+  attr_accessor :description, :effort, :task_bookings
   
   def initialize(description, effort)
     @description = description
     @effort = effort
+    @task_bookings = []
   end
 end
 
 
 class TaskBooking
-  attr_reader :task, :hours
+  attr_reader :task, :hours, :day_booking
 
-  def initialize(task, hours)
+  def initialize(task, hours, day_booking)
     @task = task
+    task.task_bookings << self
+    
     @hours = hours
+    @day_booking = day_booking
   end
 end
 
@@ -64,10 +68,37 @@ class DayBooking
   
   def book_task(task, remaining_effort)
     hours = [self.available_hours, remaining_effort].min
-    @task_bookings << TaskBooking.new(task, hours)
+    @task_bookings << TaskBooking.new(task, hours, self)
     @booked_hours += hours
     
     return remaining_effort - hours
+  end
+end
+
+
+class Plan
+  attr_accessor :project, :bookings
+  
+  def initialize(project, bookings)
+    @project = project
+    @bookings = bookings
+  end
+  
+  def export_bookings
+    @bookings.each do |b|
+      day = b.date.strftime('%Y-%m-%d')
+      b.task_bookings.each do |t|
+        puts "#{day}\t#{t.hours}\t#{t.task.description}"
+      end
+    end
+  end
+  
+  def export_dates
+    @project.tasks.each do |t|
+      first_day = t.task_bookings.first.day_booking.date
+      last_day = t.task_bookings.last.day_booking.date
+      puts "#{t.description}\t#{t.effort}\t#{first_day}\t#{last_day}"
+    end
   end
 end
 
@@ -76,6 +107,8 @@ class Project
   include Singleton
 
   attr_accessor :start, :daily_working_hours
+  
+  attr_reader :tasks
   
   def initialize
     @tasks = []
@@ -107,17 +140,7 @@ class Project
       end while remaining_effort > 0
     end
     
-    return bookings
-  end
-end
-
-
-def export_bookings(bookings)
-  bookings.each do |b|
-    day = b.date.strftime('%Y-%m-%d')
-    b.task_bookings.each do |t|
-      puts "#{day}\t#{t.hours}\t#{t.task.description}"
-    end
+    return Plan.new(self, bookings)
   end
 end
 
@@ -139,5 +162,7 @@ end
 
 load 'example'
 
-bookings = Project.instance.plan
-export_bookings(bookings)
+plan = Project.instance.plan
+plan.export_bookings
+puts
+plan.export_dates
